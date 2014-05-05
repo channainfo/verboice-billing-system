8f20d0253672c881eff36edfe812f5f4ea496402
Channa Ly <channa.info@gmail.com>
2014-05-05 16:15:56 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VerboiceBillingSystem::Application.config.secret_key_base = 'ebad39afa52c7b690b58d4204bf347eac5c88dd18b26e4dd785a9a50f814ba35fba34befe2547602db462f87c75d39b5c3dd52872bcb1b7c972000da784871fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VerboiceBillingSystem::Application.config.session_store :cookie_store, key: '_verboice_billing_system_session'
